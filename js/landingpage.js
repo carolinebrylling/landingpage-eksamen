b5840c95c98807bce13a260e3ab1fe7e1b9e4423
carolinebrylling <bryllingcaroline@gmail.com>
2026-05-19 18:53:29 +0200

    js on three buttons

--- js/landingpage.js
+++ js/landingpage.js
@@ -1,2 +1,28 @@
 "use strict";
 
+console.log("JavaScript virker");
+
+const buyBtn = document.querySelector("#buyBtn");
+const readBtn = document.querySelector("#readBtn");
+const exploreBtn = document.querySelector("#exploreBtn");
+
+// buyBtn
+buyBtn.addEventListener("click", () => {
+  alert("Tak for din interesse! Produktet er lagt i kurven.");
+});
+
+// readBtn
+readBtn.addEventListener("click", () => {
+  document.querySelector(".features").scrollIntoView({
+    behavior: "smooth",
+    block: "start"
+  });
+});
+
+// exploreBtn
+exploreBtn.addEventListener("click", () => {
+  document.querySelector(".color-section").scrollIntoView({
+    behavior: "smooth",
+    block: "start"
+  });
+});
