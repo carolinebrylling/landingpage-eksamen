a66858fa2cee5269f757d48db513da41bc7c85fe
carolinebrylling <bryllingcaroline@gmail.com>
2026-05-20 16:44:58 +0200

    made my const in js for my switch section

--- js/landingpage.js
+++ js/landingpage.js
@@ -27,4 +27,34 @@ exploreBtn.addEventListener("click", () => {
   });
 });
 
+// my products
+const products = [
+  {
+    name: "Lumina White",
+    color: "Moonlight White",
+    image: "img/moonlight-white.png",
+    alt: "Lumina højtaler i Moonlight White"
+  },
+
+  {
+    name: "Lumina Pink",
+    color: "Dusty Pink",
+    image: "img/dusty-pink.png",
+    alt: "Lumina højtaler i Dusty Pink"
+  },
+
+  {
+    name: "Lumina Green",
+    color: "Saga Green",
+    image: "img/saga-green.png",
+    alt: "Lumina højtaler i Saga Green"
+  },
+
+  {
+    name: "Lumina Mist",
+    color: "Lavender Mist",
+    image: "img/lavender-mist.png",
+    alt: "Lumina højtaler i Lavender Mist"
+  }
+];
 
