1545cdf34e26d8307a5646463f48fa1764dc87f3
carolinebrylling <bryllingcaroline@gmail.com>
2026-05-21 14:29:27 +0200

    changes some colors

--- js/landingpage.js
+++ js/landingpage.js
@@ -1,15 +1,7 @@
 "use strict";
 
-console.log("JavaScript virker");
-
-const buyBtn = document.querySelector("#buyBtn");
 const readBtn = document.querySelector("#readBtn");
 const exploreBtn = document.querySelector("#exploreBtn");
-
-// buyBtn
-buyBtn.addEventListener("click", () => {
-  alert("Tak for din interesse! Produktet er lagt i kurven.");
-});
 
 // readBtn
 readBtn.addEventListener("click", () => {
